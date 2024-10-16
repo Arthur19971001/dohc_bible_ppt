fc225e0099e6facfd8ad54ef0d9ce6499b7ffbf1
arthur.kim <arthur.kim@theta.co.nz>
2024-10-16 22:06:30 +1300

    repalce desktop_window to window_manager

--- lib/widgetbook.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'src/widgetbook/widgetbook_app.dart';
-
-void main() {
-  // Widgetbook does not require a provider scope
-  // ignore: missing_provider_scope
-  runApp(const WidgetbookApp());
-}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import desktop_window
 import path_provider_foundation
+import screen_retriever
+import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  DesktopWindowPlugin.register(with: registry.registrar(forPlugin: "DesktopWindowPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
